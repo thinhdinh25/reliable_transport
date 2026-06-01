01aa7555d5d9d6804683a74988f1080e05f3c03e
thinhdinh25 <75488819+thinhdinh25@users.noreply.github.com>
2026-06-01 19:42:40 +0700

    Add start machanism

--- RTP-base/receiver.py
+++ RTP-base/receiver.py
@@ -4,42 +4,72 @@ import socket
 from utils import PacketHeader, compute_checksum
 
 
+sender_ip = "127.0.0.1"  
+receiver_ip = "127.0.0.1"
+receiver_port = 10000
+sender_port = 10001
+
+def verify_checksum(pkt):
+    pkt_checksum_saved = pkt.checksum
+    pkt.checksum = 0
+    computed_checksum = compute_checksum(pkt)
+    return computed_checksum == pkt_checksum_saved
+
+
 def receiver(receiver_ip, receiver_port, window_size):
     """TODO: Listen on socket and print received message to sys.stdout."""
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind((receiver_ip, receiver_port))
+    state = "START"
     while True:
-        # Receive packet; address includes both IP and port
-        pkt, address = s.recvfrom(2048)
+        if state == "START":
+            # Receive packet; address includes both IP and port
+            start_pkt, address = s.recvfrom(2048)
 
-        # Extract header and payload
-        pkt_header = PacketHeader(pkt[:16])
-        msg = pkt[16 : 16 + pkt_header.length]
+            # Extract header and payload
+            start_pkt = PacketHeader(start_pkt[:16])
 
-        # Verity checksum
-        pkt_checksum = pkt_header.checksum
-        pkt_header.checksum = 0
-        computed_checksum = compute_checksum(pkt_header / msg)
-        if pkt_checksum != computed_checksum:
-            print("checksums not match")
-        print(msg)
+            #verify start package
+            if start_pkt.type != 0 or start_pkt.seq_num != 0 or start_pkt.length != 0:
+                print("Havent started yet")
+                continue
+
+            # Verity checksum
+            pkt_checksum = start_pkt.checksum
+            start_pkt.checksum = 0
+            computed_checksum = compute_checksum(start_pkt)
+            if pkt_checksum != computed_checksum:
+                print("checksums not match")
+                continue
+
+            # resent ack of start
+            ack_header = PacketHeader(type=3, seq_num= start_pkt.seq_num + 1, length=0)
+            ack_header.checksum = compute_checksum(ack_header)
+            s.sendto(bytes(ack_header), (sender_ip, sender_port))
+            #set new state to data
+        elif state == "DATA":
+            print(1)
+
+        
+        
+
+receiver(receiver_ip, receiver_port, 4)
+
+# def main():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument(
+#         "receiver_ip", help="The IP address of the host that receiver is running on"
+#     )
+#     parser.add_argument(
+#         "receiver_port", type=int, help="The port number on which receiver is listening"
+#     )
+#     parser.add_argument(
+#         "window_size", type=int, help="Maximum number of outstanding packets"
+#     )
+#     args = parser.parse_args()
+
+#     receiver(args.receiver_ip, args.receiver_port, args.window_size)
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "receiver_ip", help="The IP address of the host that receiver is running on"
-    )
-    parser.add_argument(
-        "receiver_port", type=int, help="The port number on which receiver is listening"
-    )
-    parser.add_argument(
-        "window_size", type=int, help="Maximum number of outstanding packets"
-    )
-    args = parser.parse_args()
-
-    receiver(args.receiver_ip, args.receiver_port, args.window_size)
-
-
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
--- RTP-base/sender.py
+++ RTP-base/sender.py
@@ -4,30 +4,83 @@ import socket
 from utils import PacketHeader, compute_checksum
 
 
+sender_ip = "127.0.0.1"  
+receiver_ip = "127.0.0.1"
+receiver_port = 10000
+sender_port = 10001
+
+text = "thisisaverylongtextthatyoucanneverimageinyourmind"
+
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+def verify_checksum(pkt):
+    pkt_checksum_saved = pkt.checksum
+    pkt.checksum = 0
+    computed_checksum = compute_checksum(pkt)
+    return computed_checksum == pkt_checksum_saved
+
+
 def sender(receiver_ip, receiver_port, window_size):
-    """TODO: Open socket and send message from sys.stdin."""
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    pkt_header = PacketHeader(type=2, seq_num=10, length=14)
-    pkt_header.checksum = compute_checksum(pkt_header / "Hello, world!\n")
-    pkt = pkt_header / "Hello, world!\n"
-    s.sendto(bytes(pkt), (receiver_ip, receiver_port))
+    #initialization
+    s.bind((sender_ip, sender_port))
+    s.settimeout(0.5)
+    sender_start()
+
+def sender_start():
+    #send start package
+    start_header = PacketHeader(type=0, seq_num=0, length=0)
+    start_header.checksum = compute_checksum(start_header)
+    s.sendto(bytes(start_header), (receiver_ip, receiver_port))
+    #check ack start
+    while True:
+        try:
+            ack, address = s.recvfrom(2048)
+        except socket.timeout:
+            # resend packet
+            print("Timed out, need to resent")
+            s.sendto(bytes(start_header), (receiver_ip, receiver_port))
+            continue
+            
+        # Extract header and payload
+        ack_header = PacketHeader(ack[:16])
+
+        #verify ack detail
+        if ack_header.type != 3 or ack_header.seq_num != 1 or ack_header.length != 0:
+            print("Not ACK")
+            continue
+
+        # Verify checksum
+        if verify_checksum(ack_header) == True:
+            print("ACK checksums not match")
+            continue
+        break
+
+def sender_data():
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "receiver_ip", help="The IP address of the host that receiver is running on"
-    )
-    parser.add_argument(
-        "receiver_port", type=int, help="The port number on which receiver is listening"
-    )
-    parser.add_argument(
-        "window_size", type=int, help="Maximum number of outstanding packets"
-    )
-    args = parser.parse_args()
-
-    sender(args.receiver_ip, args.receiver_port, args.window_size)
 
 
-if __name__ == "__main__":
-    main()
+
+sender(receiver_ip, receiver_port, 4)
+
+
+# def main():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument(
+#         "receiver_ip", help="The IP address of the host that receiver is running on"
+#     )
+#     parser.add_argument(
+#         "receiver_port", type=int, help="The port number on which receiver is listening"
+#     )
+#     parser.add_argument(
+#         "window_size", type=int, help="Maximum number of outstanding packets"
+#     )
+#     args = parser.parse_args()
+
+#     sender(args.receiver_ip, args.receiver_port, args.window_size)
+
+
+# if __name__ == "__main__":
+#     main()
